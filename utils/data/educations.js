26a5d51592f46d441380c6bd89995755df71a6f8
Raihan Fathurrahman <cahtlogosari@gmail.com>
2024-05-07 17:20:31 +0700

    new

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,32 +1,26 @@
-import myskill from '/public/image/myskill.png';
+
 import udinus from '/public/image/udinus.png';
 import il from '/public/image/il.png';
 import smk from '/public/image/smk8.png';
 
 export const educations = [
+
   {
     id: 1,
-    logo: myskill,
-    title: "Data Analysis: Fullstack Intensive Bootcamp",
-    duration: "March – May 2024",
-    institution: "MySkill",
-  },
-  {
-    id: 2,
     logo: udinus,
     title: "S1 Informatics Engineering",
     duration: "June 2020 – February 2024",
     institution: "Universitas Dian Nuswantoro",
   },
   {
-    id: 3,
+    id: 2,
     logo: il,
     title: "Independent Study Program Batch 3 on Web Development Bootcamp",
     duration: "September 2022 - January 2023",
     institution: "Infinite Learning Indonesia",
   },
   {
-    id: 4,
+    id: 3,
     logo: smk,
     title: "Vocational degree, Software Engineering",
     duration: "July 2017 – May 2020",
