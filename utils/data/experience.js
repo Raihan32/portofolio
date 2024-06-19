e6b921d5d3ab996e07354849d4ece79a62a47948
Raihan Fathurrahman <cahtlogosari@gmail.com>
2024-06-19 22:15:41 +0700

    new experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -6,13 +6,21 @@ export const experiences = [
   {
     id: 1,
     logo: UT,
+    title: 'Cyber Security Analyst - SOC L1',
+    company: "PT. Sembilan Pilar Semesta",
+    duration: "(May 2024)",
+    des: "Monitoring: Continuously monitor and oversee the security environment using Stellar Cyber to detect and respond to potential threats and vulnerabilities.Ticketing: Create, manage, and resolve tickets related to security alerts and incidents. Ensure accurate documentation and timely escalation of issues when necessary.Reporting: Develop and deliver detailed reports on security alerts and incidents. Provide insights and recommendations based on data analysis to improve overall security posture.Incident Analysis: Investigate, analyze, and respond to security incidents. Conduct root cause analysis to identify the underlying issues and implement corrective actions to prevent recurrence.."
+  },
+  {
+    id: 2,
+    logo: UT,
     title: 'Data Analyst SRCOM',
     company: "PT United Tractors Tbk",
     duration: "(Feb 2023 - June 2023)",
     des: "As a Data Analyst, I am responsible for gathering, analyzing, and interpreting relevant data for the company. I can organize data and utilize analysis tools such as Excel and Spreadsheets to obtain accurate information. My skills also include identifying trends, providing recommendations based on data analysis, and visualizing data using Google Data Studio. I have effective communication skills to convey analysis results and collaborate with other teams. Additionally, I continuously update my skills by keeping up with technological advancements in data analysis."
   },
   {
-    id: 2,
+    id: 3,
     logo: UT,
     title: 'Web Developer SRCOM',
     company: "PT United Tractors Tbk",
@@ -20,7 +28,7 @@ export const experiences = [
     des: "As a Web Developer at SRCOM in PT United Tractors, my responsibility involves designing and developing warehouse websites utilizing advanced technologies like Next.js and Express.js. I excel in organizing and overseeing all facets of web development, from conceptualization to execution. Leveraging my proficiency in Next.js, I craft responsive and dynamic user interfaces to deliver an exceptional user experience. Additionally, I utilize Express.js to establish a robust server-side logic foundation, ensuring optimal performance for the warehouse website."
   },
   {
-    id: 3,
+    id: 4,
     logo: ikitas,
     title: "Front-End Developer",
     company: "Inkubator Kreasi Dan Inovasi Telematika Semarang",
